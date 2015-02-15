cdeae3c2d4df6b02211eea6db88dc7c34cafa112
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2015-02-15 19:25:49 +0100

    Mark Python 2.7 as supported
    Since all the tests seem to pass for me, on Python 2.7, I'm marking it
    as supported. All of setup.py meta-data, tox and travis configuration
    files are updated. Lastly, the README file is updated to indicate that
    both major versions are now supported
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- setup.py
+++ setup.py
@@ -48,6 +48,8 @@ setup(
         ('License :: OSI Approved :: GNU Lesser General Public License v3'
          ' (LGPLv3)'),
         'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
