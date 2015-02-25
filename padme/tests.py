2124166e4734a24567741658490c1ad4fe099bfc
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2015-02-25 18:31:52 +0100

    Add a short docstring to padme.tests
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- padme/tests.py
+++ padme/tests.py
@@ -15,6 +15,13 @@
 #
 # You should have received a copy of the GNU Lesser General Public License
 # along with Padme.  If not, see <http://www.gnu.org/licenses/>.
+"""
+:mod:`padme.tests` -- tests for padme
+=====================================
+
+This package contains unit tests for padme. You don't want to use it directly
+and it can get changed at any time without notice.
+"""
 from __future__ import print_function, absolute_import, unicode_literals
 
 import doctest
